d568d116eca52464dc2375a025317405948a5c20
lee0119 <candogo91@gmail.com>
2022-09-07 19:26:17 +0900

    ing...

--- src/main/java/com/example/soso/configuration/CorsConfig.java
+++ src/main/java/com/example/soso/configuration/CorsConfig.java
@@ -1,6 +1,6 @@
 package com.example.soso.configuration;
 
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,26 +13,27 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("*")
+                .allowedOrigins("http://localhost:3000")
+                .exposedHeaders("Authorization", "RefreshToken")
+                .allowCredentials(true)
                 .maxAge(3000);
 
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("http://1336.s3-website.ap-northeast-2.amazonaws.com")
-                        .exposedHeaders("Authorization", "RefreshToken")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedOrigins("http://1336.s3-website.ap-northeast-2.amazonaws.com")
+//                        .exposedHeaders("Authorization", "RefreshToken")
+//                        .allowedMethods("*")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true)
+//                        .maxAge(3600);
+//            }
+//        };
     }
 
-
-}
